9c0646293fa850a4c12a339d4598390ed0fcd671
wengzepeng <wengzepeng@pcitech.com>
2020-09-08 16:52:07 +0800

    wzp  增加Object2String 转换器

--- EfficientDesigner_Control/Converters/Converter.cs
+++ EfficientDesigner_Control/Converters/Converter.cs
@@ -12,11 +12,23 @@ namespace EfficientDesigner_Control.Converters
     public class CollapsedWhenFalse : IValueConverter
     {
         public static readonly CollapsedWhenFalse Instance = new CollapsedWhenFalse();
-        
+
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             return (bool)value ? Visibility.Visible : Visibility.Collapsed;
         }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Object2String : IValueConverter
+    {
+        public static readonly Object2String Instance = new Object2String();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => value?.ToString();
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
