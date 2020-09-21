a940c00d12740ab187d7734958f8fc91d59909c4
wengzepeng <wengzepeng@pcitech.com>
2020-09-21 17:30:37 +0800

    wzp  提交实体

--- EfficientDesigner_Control/Models/MasterDetailModel.cs
+++ EfficientDesigner_Control/Models/MasterDetailModel.cs
@@ -8,5 +8,15 @@ namespace EfficientDesigner_Control.Models
 {
     class MasterDetailModel
     {
+        public string DisplayName { get; }
+        public MasterModel Master { get; }
+        public List<MasterDetailModel> Detail { get; }
+
+        public MasterDetailModel(string displayName, MasterModel master, List<MasterDetailModel> detail)
+        {
+            DisplayName = displayName;
+            Master = master;
+            Detail = detail ?? new List<MasterDetailModel>();
+        }
     }
 }
--- EfficientDesigner_Control/Models/MasterModel.cs
+++ EfficientDesigner_Control/Models/MasterModel.cs
@@ -8,5 +8,13 @@ namespace EfficientDesigner_Control.Models
 {
     class MasterModel
     {
+        public MasterModel(string displayName, List<MasterDetailModel> details = null)
+        {
+            DisplayName = displayName;
+            Details = details ?? new List<MasterDetailModel>();
+        }
+
+        public string DisplayName { get; }
+        public List<MasterDetailModel> Details { get; }
     }
 }
