6da117ad07344f941560384a71c5156e1e46dc66
wengzepeng <wengzepeng@pcitech.com>
2020-10-22 17:21:41 +0800

    添加 shell 程序。

---
+++ EfficientDesigner_Shell/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using EfficientDesigner_Service.ServiceImplements;
+using Prism.Ioc;
+using Prism.Unity;
+using EfficientDesigner_Shell.Views;
+using EfficientDesigner_Service.Services;
+
+namespace EfficientDesigner_Shell
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.Register<ILayoutService, LayoutService>();
+        }
+
+        protected override Window CreateShell() => new MainWindow();
+    }
+}
