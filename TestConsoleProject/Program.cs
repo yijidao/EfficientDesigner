d2ffc4880d5f876621d8420dcc362542288ca3d2
wengzepeng <547202221@qq.com>
2020-11-24 23:16:26 +0800

    提交测试程序

--- TestConsoleProject/Program.cs
+++ TestConsoleProject/Program.cs
@@ -18,7 +18,8 @@ namespace TestConsoleProject
             //ServiceFactory.GetLayoutService().UpdateDataSource(new DataSource{Api = "https://localhost:5001/efficientdesignapi/GetMetro" });
             //ServiceFactory.GetLayoutService().UpdateDataSource(new DataSource{Api = "https://localhost:5001/efficientdesignapi/GetPCI" });
             //ServiceFactory.GetLayoutService().UpdateDataSource(new DataSource{Api = "https://localhost:5001/efficientdesignapi/GetImage" });
-            ServiceFactory.GetLayoutService().UpdateDataSource(new DataSource { Api = "https://localhost:5001/efficientdesignapi/GetPassengerFlow" });
+            //ServiceFactory.GetLayoutService().UpdateDataSource(new DataSource { Api = "https://localhost:5001/efficientdesignapi/GetPassengerFlow" });
+            ServiceFactory.GetLayoutService().UpdateDataSource(new DataSource { Api = "https://localhost:5001/efficientdesignapi/GetInOutPassengerFlow" });
 
             Console.WriteLine("添加完毕...");
         }
