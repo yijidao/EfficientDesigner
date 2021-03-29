bf6f10ba963f2163e8dbc244a545cb0609643a08
wengzepeng <wengzepeng@pcitech.com>
2021-03-29 18:16:41 +0800

    添加服务面板

---
+++ EfficientDesigner_Control/ViewModels/ServicePanel/ServiceItemViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Mvvm;
+
+namespace EfficientDesigner_Control.ViewModels.ServicePanel
+{
+    public class ServiceItemViewModel : BindableBase
+    {
+        
+    }
+}
