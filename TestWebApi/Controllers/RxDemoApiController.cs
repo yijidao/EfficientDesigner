71b080429a6cc742b3a937e79dda3bdd6afccfef
wengzepeng <547202221@qq.com>
2021-04-06 23:42:13 +0800

    1. 提交 RxDemoApiController。 2. 提交Dto。（感觉写法不太行）

---
+++ TestWebApi/Controllers/RxDemoApiController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using TestWebApi.Dto;
+
+namespace TestWebApi.Controllers
+{
+    [ApiController]
+    [Route("[rxapi/controller]")]
+    public class RxDemoApiController : ControllerBase
+    {
+        public RxDemoApiController(ILogger<RxDemoApiController> logger)
+        {
+
+        }
+
+        [HttpGet("Login")]
+        public string Login([FromBody] LoginRequest request)
+        {
+            if (request.Username.Trim() != "admin" || request.Password.Trim() != "admin")
+            {
+                return JsonConvert.SerializeObject(ResponseBase.CreateErrorResponse("账号或密码错误"));
+            }
+            var dic = new Dictionary<string, string> { { "userId", "001" } };
+            
+            return JsonConvert.SerializeObject(dic);
+        }
+    }
+}
---
+++ TestWebApi/Dto/ResponseBase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Threading.Tasks;
+
+namespace TestWebApi.Dto
+{
+
+    public class ResponseBase
+    {
+        public ErrorModel Error { get; set; }
+
+        public static ResponseBase CreateErrorResponse(string message) => new ResponseBase { Error = new ErrorModel { Message = message } };
+    }
+
+    public class ResponseBase<T> : ResponseBase
+    {
+        public T Data { get; set; }
+    }
+
+    public class ErrorModel
+    {
+        public string Message { get; set; }
+    }
+}
